68311361284222e7b93e5b4eac473d3f6ac177ff
Nata <nata3010@list.ru>
2023-04-22 11:09:20 +0300

    add folder Example002

--- Example/Example002/obj/Debug/net6.0/Example002.AssemblyInfo.cs
+++ Example/Example002/obj/Debug/net6.0/Example002.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Example002")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("C#")]
-[assembly: System.Reflection.AssemblyTitleAttribute("C#")]
+[assembly: System.Reflection.AssemblyProductAttribute("Example002")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Example002")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
